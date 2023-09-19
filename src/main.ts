aeb0e22db37a08cb81faf777d719fb44ea35fe48
phamanhquyet <q24052002@gmail.com>
2023-09-19 21:21:02 +0700

    set up cookie session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['quyet'], //trong này có thể là bất cứ thứ gì mình muốn
+      //chuỗi trong keys này được sử dụng để mã hóa thông tin được lưu trữ trong cookie
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
